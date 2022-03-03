e5c9a112dd4bb274f8eb6c473ca78f1a667a7db1
roebel <axel.roebel@ircam.fr>
2022-03-03 12:01:44 +0100

    Added copyright info.

--- MBExWN_Voc/utils/math_utils.py
+++ MBExWN_Voc/utils/math_utils.py
@@ -1,15 +1,27 @@
+# AUTHOR:  A.Roebel
+# COPYRIGHT: Copyright(c) 2014 - 2022 IRCAM - Roebel
+
 from __future__ import division
 import numpy as np
+
 
 def nextpow2(n):
     '''
     return next power of 2 of such that 2**nextpow2(n) >= n
+
+
     '''
     return np.int32(np.ceil(np.log2(n)))
 
 def nextpow2_val(n):
     '''
     return integer v being a power of 2 with v >= n
+
+    AUTHOR
+      A. Roebel
+
+     COPYRIGHT
+        Copyright (c) 2012-2022 IRCAM - Roebel
     '''
     v = 2
     while v < n: v = v * 2
