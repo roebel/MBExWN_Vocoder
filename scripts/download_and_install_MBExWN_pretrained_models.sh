2a2cf29ba9b058ad9d1c6da92be129ea5ad020af
roebel <axel.roebel@ircam.fr>
2022-03-05 22:55:44 +0100

    Removed downladed zip after installing.

--- scripts/download_and_install_MBExWN_pretrained_models.sh
+++ scripts/download_and_install_MBExWN_pretrained_models.sh
@@ -5,3 +5,4 @@ cd $DIR/..
 wget https://nubo.ircam.fr/index.php/s/RHexyqmbTEWLS6w/download -O SIIConv_pretrained_MBExWN_models.zip
 
 unzip  SIIConv_pretrained_MBExWN_models.zip
+rm -f SIIConv_pretrained_MBExWN_models.zip
