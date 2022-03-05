c7056b49d5607eae7fb3d51ff384d860d98d0256
roebel <axel.roebel@ircam.fr>
2022-03-05 20:30:29 +0100

    Fixed base ParamScheduler such that it can be instantiated.

--- MBExWN_NVoc/vocoder/model/training_utils.py
+++ MBExWN_NVoc/vocoder/model/training_utils.py
@@ -12,40 +12,20 @@ import numpy as np
 import os, sys
 from copy import deepcopy
 
-
-
-class ParamSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+# this is the scheduler base class that does not support any scheduling.
+class ParamSchedule(object):
     def __init__(self, initial, name, type="constant", quiet=False, **kwargs):
         self.type = type
-
-        if type == "constant":
-            self.scheduler = PiecewiseConstantSchedule(initial=initial,  name=name, **kwargs)
-        elif type == "linear":
-            self.scheduler = PiecewiseLinearSchedule(initial=initial, name=name, **kwargs)
-        elif type == "exponential":
-            self.scheduler = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=initial,
-                                                                            name=name, **kwargs)
-        else:
-            raise RuntimeError(f"PiecewiseParamSchedule::error:: type {type} not supported. Please choose either 'constant' or 'linear'")
-
-        if not quiet:
-            if self.type == "exponential":
-                print(f"initialized exponential scheduler for parameter {self.scheduler.name} with initial {self.scheduler.initial_learning_rate} and args {kwargs}",
-                      file=sys.stderr, flush=True)
-            else:
-                print("initialized", str(self.scheduler), file=sys.stderr, flush=True)
+        self.initial = initial
+        self.name = name
 
     def __call__(self, step : tf.int64)  -> tf.float32:
-        ret = self.scheduler(step=step)
-        # tf.print("scheduler return", ret)
-        return ret
+        return self.initial
 
     def get_config(self):
-        config = self.scheduler.get_config()
-        if "initial_learning_rate" in config:
-            config.update(initial=config["initial_learning_rate"])
-            del config["initial_learning_rate"]
-        config.update(type= self.type)
+        config["initial"] = self.initial
+        config["type"] = self.type
+        config["name"] = self.name
         return config
 
 
